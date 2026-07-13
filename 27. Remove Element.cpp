c6259ada11879a1143d100db176d4492223198b1
Vivek Jangir <2025uec1862@mnit.ac.in>
2026-07-13 22:27:33 +0530

    Create 27. Remove Element.cpp

---
+++ 27. Remove Element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int n = nums.size();
+
+        int j=0;
+
+        for(int i =0; i<n;i++){
+            if(nums[i] !=val){
+                if(i!=j){
+                    swap(nums[i], nums[j]);
+                }
+                j++;
+            }
+        }
+        return j;
+    }
+};
